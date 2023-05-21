5e9171acf6b93add75fea1d300e21b61de3bee94
toc <tomvarewyck98@gmail.com>
2023-05-21 18:04:21 +0200

    feat: creating register page

---
+++ src/app/pages/auth/register/register.page.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, Validators } from '@angular/forms';
+import { IonicModule, NavController } from '@ionic/angular';
+import { InputComponent } from '../../../components/input/input.component';
+import { ButtonComponent } from '../../../components/button/button.component';
+import { BackButtonComponent } from '../../../components/back-button/back-button.component';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: true,
+  imports: [
+    IonicModule,
+    CommonModule,
+    InputComponent,
+    ButtonComponent,
+    BackButtonComponent
+  ]
+})
+export class RegisterPage implements OnInit {
+
+  emailFormControl = new FormControl(null, [Validators.required]);
+  passwordFormControl = new FormControl(null, [Validators.required]);
+
+  get isFormValid(): boolean {
+    return this.emailFormControl.valid && this.passwordFormControl.valid;
+  }
+
+  constructor(private navController: NavController) { }
+
+  ngOnInit() {
+  }
+
+  onBack() {
+    this.navController.back({animationDirection: 'back'});
+  }
+
+  onRegister() {
+    if (this.isFormValid) this.navController.navigateForward('home', {animationDirection: 'forward'});
+  }
+}
