ad6066d1e00aa73c0a7b11049e673a5ddc1f3c32
toc <tomvarewyck98@gmail.com>
2023-05-21 18:10:57 +0200

    feat: startup app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [
+    IonicModule,
+    CommonModule
+  ],
 })
 export class AppComponent {
-  constructor() {}
+
+  isDarkTheme = false;
+
+  constructor(private renderer: Renderer2) {}
+
+  ngOnInit() {
+    // const isDarkTheme = JSON.parse(localStorage.getItem('color-theme')?? '');
+    // this.isDarkTheme = !!isDarkTheme;
+    this.onSetColorTheme(false);
+  }
+
+  onSetColorTheme(isDark: boolean) {
+    this.renderer.setAttribute(document.body, 'color-theme', isDark ? 'dark': 'light');
+  }
+
+  onToggleColorTheme(event: any) {
+    localStorage.setItem('color-theme', JSON.stringify(event.detail.checked));
+    this.onSetColorTheme(event.detail.checked);
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,28 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('././pages/home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () => import('./pages/auth/login/login.page').then(m => m.LoginPage)
+      },
+      {
+        path: 'register',
+        loadComponent: () => import('./pages/auth/register/register.page').then( m => m.RegisterPage)
+      },
+      {
+        path: '',
+        loadComponent: () => import('././pages/auth/auth.page').then(m => m.AuthPage)
+      }
+    ]
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'auth',
     pathMatch: 'full',
-  },
+  }
 ];
