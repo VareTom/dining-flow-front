e3be288fc39b84f5c40c7f099594d705fdcb0366
toc <tomvarewyck98@gmail.com>
2023-05-21 20:59:29 +0200

    feat: Adding mandatory field error message

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
@@ -13,13 +13,22 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   ]
 })
 export class InputComponent  implements OnInit {
+  @ViewChild('input') input!: ElementRef;
 
   @Input() formControl!: FormControl;
   @Input() label!: string;
   @Input() type!: string;
 
+  get isFieldRequired(): boolean {
+    return this.formControl.touched && this.formControl.invalid && this.formControl.getError('required');
+  }
+
   constructor() { }
 
   ngOnInit() {}
+
+  onFocus() {
+    this.input.nativeElement.focus();
+  }
 
 }
